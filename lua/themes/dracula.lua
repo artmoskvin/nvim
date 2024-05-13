74afd08c32c1e3a0a5b1f9e4ebabcf4d97aa732c
Art Moskvin <amoskvin@me.com>
2024-05-13 16:40:46 +0200

    Add supermaven; configure dracula

--- lua/themes/dracula.lua
+++ lua/themes/dracula.lua
@@ -3,6 +3,15 @@ return {
     'Mofiqul/dracula.nvim',
     priority = 1000,
     init = function()
+      local dracula = require 'dracula'
+      local colors = dracula.colors()
+      dracula.setup {
+        overrides = {
+          LspReferenceText = { bg = colors.visual },
+          LspReferenceRead = { bg = colors.visual },
+          LspReferenceWrite = { bg = colors.visual },
+        },
+      }
       vim.cmd.colorscheme 'dracula'
     end,
   },
