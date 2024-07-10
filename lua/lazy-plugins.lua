1348e371eecb03f7706b9d6d6e657d8a920bcafe
Art Moskvin <amoskvin@me.com>
2024-07-10 19:45:43 +0200

    Add codesnap

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -75,6 +75,8 @@ require('lazy').setup({
 
   -- Disabling because it's not working
   -- require 'plugins.neotest',
+
+  require 'plugins.codesnap',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,9 @@
+return {
+  'mistricky/codesnap.nvim',
+  build = 'make',
+  keys = {
+    { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'CodeSnap: Save to clipboard' },
+    { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'CodeSnap: Save to ~/Pictures' },
+  },
+  opts = { watermark = '' },
+}
